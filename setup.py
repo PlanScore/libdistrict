7e23b6b0d3f52a0e1b0075dd3f579a1d6211a9e1
Becky Andresen <rdandresen@gmail.com>
2018-03-17 11:24:09 -0500

    Checkpoint commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'libdistrict',
+    version = '0.1.0',
+    description = 'Library of common metrics used to evaluate district plans',
+    url = 'https://github.com/PlanScore/libdistrict',
+    license = 'MIT',
+    packages = [],
+)
