2ae8d8467b6ec5b4e0f80c49831b16b3a18c3446
Becky Andresen <rdandresen@gmail.com>
2018-04-10 09:12:46 -0500

    Updated the mean-median tests and metric to calculate the median - mean vote share (%).

--- src/libdistrict/partisan_symmetry.py
+++ src/libdistrict/partisan_symmetry.py
@@ -35,31 +35,33 @@ def efficiency_gap(district_plan, party_a, party_b):
 
 
 
-def mean_median_diff(district_plan, party):
+def mean_median_diff(district_plan, party_to_analyze, competing_party):
     """
     :param district_plan: an iterable of Districts
-    :param party: the key specifying which party to analyze
+    :param party_to_analyze: the key specifying which party to analyze
+    :param competing_party: the competing party
     :return: median - mean number of votes across all districts
-    a negative score means the analyzed party has an advantage
-    a positive score means the analyzed party has a disadvantage
+    a negative score means the analyzed party may have a disadvantage
+    a positive score means the analyzed party may have an advantage
     """
 
     is_district_plan(district_plan)
 
-    votes_per_district = []
+    vote_share_per_district = []
 
     # store all votes for party in a sequence
     for district in district_plan:
-        party_votes = district.party_votes[party]
-        votes_per_district.append(party_votes)
+        party_votes = district.party_votes[party_to_analyze]
+        total_votes = district.party_votes[party_to_analyze] + district.party_votes[competing_party]
+        vote_share_per_district.append(party_votes/total_votes)
 
-    # get the median votes for a district
-    party_votes_median = statistics.median(votes_per_district)
+    # get the median votes for all districts for the party
+    party_share_median = statistics.median(vote_share_per_district)
 
-    # get the mean for the district
-    party_votes_mean = statistics.mean(votes_per_district)
+    # get the mean for all districts for the party
+    party_share_mean = statistics.mean(vote_share_per_district)
 
-    return party_votes_median - party_votes_mean
+    return party_share_median - party_share_mean
 
 def is_district(district):
     if not isinstance(district, District):
--- tests/test_partisan_symmetry.py
+++ tests/test_partisan_symmetry.py
@@ -68,27 +68,40 @@ class TestMeanMedianDifference(unittest.TestCase):
         self.district_plan = {district1, district2, district3, district4, district5,
                                 district6, district7, district8, district9, district10}
 
+
+        district11 = District(id=11, party_votes={'partyA': 6, 'partyB': 5})
+        district12 = District(id=12, party_votes={'partyA': 6, 'partyB': 5})
+        district13 = District(id=13, party_votes={'partyA': 4, 'partyB': 5})
+        district14 = District(id=14, party_votes={'partyA': 4, 'partyB': 8})
+        district15 = District(id=15, party_votes={'partyA': 4, 'partyB': 8})
+
+        self.district_plan_2 = {district11, district12, district13, district14, district15}
+
         self.key_a = 'partyA'
         self.key_b = 'partyB'
 
-    def test_MMD_partyA(self):
+    def test_mmd_party_a_disadvantage(self):
 
-        self.assertEqual(-1, mean_median_diff(self.district_plan, self.key_a))
+        self.assertAlmostEqual(-.01, mean_median_diff(self.district_plan, self.key_a, self.key_b), places=10)
 
-    def test_MMD_partyB(self):
+    def test_mmd_party_b_advantage(self):
 
-        self.assertEqual(1, mean_median_diff(self.district_plan, self.key_b))
+        self.assertAlmostEqual(.01, mean_median_diff(self.district_plan, self.key_b, self.key_a), places=10)
 
-    def test_MMD_None_district_plan(self):
-        districtNone = None
+    def test_mmd_no_advantage(self):
+
+        self.assertAlmostEqual(0, mean_median_diff(self.district_plan_2, self.key_a, self.key_b), places=2)
+
+    def test_mmd_none_district_plan(self):
+        district_none = None
 
         with self.assertRaises(TypeError):
-            efficiency_gap(districtNone, self.key_a, self.key_b)
+            mean_median_diff(district_none, self.key_a, self.key_b)
 
-    def test_MMD_bad_district_plan(self):
+    def test_mmd_bad_district_plan(self):
         district1 = District(id=1, party_votes={'partyA': 70, 'partyB': 30})
         district2 = District(id=2, party_votes={'partyA': 70, 'partyB': 30})
         district_plan = {district1, "not a district", district2}
 
         with self.assertRaises(TypeError):
-            efficiency_gap(district_plan, self.key_a, self.key_b)
+            mean_median_diff(district_plan, self.key_a, self.key_b)
